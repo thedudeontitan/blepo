b7bb3711b4c239ee0ef26feb6277568e1c13406f
Ankit Kokane <ankitkokane90@gmail.com>
2024-08-26 21:02:56 +0530

    added opBnb stats APIs

---
+++ src/app/api/opBnbStats/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const apiUrl = `${process.env.OPBNB_EXPLORER_URL}/statistics/getStatisticsData`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const transferDayCount = data.data.transferDayCount;
+    return NextResponse.json({ transferDayCount });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+  }
+}
